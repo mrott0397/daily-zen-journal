8eb7f5420b6a26b0a5f49c3420d087b1689b8821
jrob02180 <jrob02180@gmail.com>
2023-06-03 17:57:26 -0600

    Fixed login to route to profile page.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,7 +35,7 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+    window.location.assign('/profile');
   }
 
   logout() {
