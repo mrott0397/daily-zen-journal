88849faeb10e8c31cb3073793d9d37b7ffece48f
WalleyemanZ <jacobzea96@gmail.com>
2023-06-05 19:35:04 -0600

    got journal entries to save on the back end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,7 +40,7 @@ const resolvers = {
       if (context.user) {
         return await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { journalEntries: {...thoughtData}} },
+          { $addToSet: { journalEntries: thoughtData } },
           { new: true }
         );
       }
